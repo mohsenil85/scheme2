bf071ffe44eb69a478b2a566b04326429a8fbcf0
Logan Mohseni <mohsenil85@gmail.com>
2014-02-17 13:20:24 -0800

    dumpin cores

--- src/parse.c
+++ src/parse.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct node{
+  char * data[64];
+} Node;
 
 
 char* parse(char* str){
   int i;
   for (i = 0; i < strlen(str); i++){
-    printf("%c\n", str[i]);
+    if (strcmp(str[i], "(")== 0){
+      printf("open paren detected");
+    } else if (str[i] == ")"){
+      printf("close paren detected");
+    } else {
+      printf("%c\n", str[i]);
+    }
   }
 
   return str;
