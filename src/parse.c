0e566c0f3e271add35433d7d4bb4c970ea377982
mohsenil85 <mohsenil85@gmail.com>
2014-02-17 19:38:45 -0800

    changed scanning strategy

--- src/parse.c
+++ src/parse.c
@@ -1,35 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct node{
-  char * data[64];
-} Node;
-
-
 char* parse(char* str){
-  int i;
-  for (i = 0; i < strlen(str); i++){
-    char* return_str[64];
-    char letter = str[i];
-    switch(letter){
-      case 40:
-        printf("open paren detected\n");
-        break;
-      case 41:
-        printf("close paren detected\n");
-        break;
-      case 32:
-        printf("space detected\n");
-        break;
-      default:
-        strcat(return_str, letter);
-        //printf("letter = %c\n", letter);
-        //printf("letter as digit = %d\n", letter);
-        break;
+  while (*str++){
+    printf("%c\n", str[0]);
 
-    }
-  return return_str;
   }
-
   return str;
 }
