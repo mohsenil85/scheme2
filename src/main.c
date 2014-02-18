8f8711a99146f44833dd74d89068923cce5588f7
Logan Mohseni <mohsenil85@gmail.com>
2014-02-18 12:44:35 -0800

    no idea what changed

--- src/main.c
+++ src/main.c
@@ -2,7 +2,8 @@
 #include "parse.h"
 
 int main(){
-  char test_string[100] = "(test1 (test2 test3 test4))";
+ // char test_string[100] = "(test1 (test2 test3 test4))";
+  char test_string[100] = "(* (+ 2 2) (+ 3 3))";
   char* out_str = parse(test_string);
   printf("output of parse:  %s\n", out_str);
   return 0;
--- src/parse.c
+++ src/parse.c
@@ -2,13 +2,14 @@
 #include <string.h>
 #include <src/stack.h>
 
-
+Stack parse_stack;
+Stack eval_stack;
 char* parse(char* str){
   char copy_of_string[strlen(str)];
   strncpy(copy_of_string, str, strlen(str));
 
   while (*str++){
-    printf("%c\n", str[0]);
+    printf("%c", str[0]);
 
   }
   return str;
