17ae2076cc3ec4580e5017013f6892527c3a1c4d
mohsenil85 <mohsenil85@gmail.com>
2014-02-18 14:02:27 -0800

    messed up handle open paren fun

--- src/parse.c
+++ src/parse.c
@@ -4,6 +4,12 @@
 
 Stack parse_stack;
 Stack eval_stack;
+
+void handle_open_paren(int data){
+  stack_push(&parse_stack, data);
+  char* output = stack_pop(&parse_stack);
+  printf("poped: %c", &output);
+}
 char* parse(char* str){
   char copy_of_string[strlen(str)];
   strncpy(copy_of_string, str, strlen(str));
@@ -12,6 +18,7 @@ char* parse(char* str){
     printf("%c ", str[i]);
     char* letter = &str[i];
     if (strncmp(letter, "(", 1) == 0){
+      handle_open_paren((int) letter);
       printf("open paren detected\n");
     }
     if (strncmp(letter, ")", 1) == 0){
