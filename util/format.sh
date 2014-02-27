f037b0ab82e850f35718ec6775935190e4bbe601
Logan Mohseni <mohsenil85@gmail.com>
2014-02-27 09:35:31 -0800

    Update format.sh

--- util/format.sh
+++ util/format.sh
@@ -1,6 +1,7 @@
-cd ../src;
-for i in `ls ../src`; do 
-  cp $i tmp.$i.c;
-  clang-format tmp.$i.c > $i;
-done;
-rm tmp.*;
+echo "are you really sure you have clang-format?"
+#cd ../src;
+#for i in `ls ../src`; do 
+#  cp $i tmp.$i.c;
+#  clang-format tmp.$i.c > $i;
+#done;
+#rm tmp.*;
